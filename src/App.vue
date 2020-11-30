63aa91a7f1fc8dcd0c5fac7ed5047913731c285d
chesh <chesh@chesh.info>
2020-11-30 13:19:36 +0000

    Initial version

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Events</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </div>
   <router-view />
 </template>
@@ -13,6 +15,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+h4 {
+  font-size: 20px;
 }
 
 #nav {
